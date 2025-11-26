0bee51d0d6e9fa3e59ba85d9f51eb10a41ca9983
santosg4edu <g.a.santos@g4educacao.com>
2025-11-26 15:18:23 -0300

    feat: implement and load supabase autentication guard

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -3,9 +3,10 @@ import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 import { RoomGateway } from './room.gateway';
 import { EventsModule } from '../events/events.module';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, SupabaseModule],
   controllers: [RoomController],
   providers: [RoomService, RoomGateway],
   exports: [RoomService],
